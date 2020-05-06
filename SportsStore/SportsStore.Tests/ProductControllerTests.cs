0d2f36d39c08faf79767d681adf955c19b018fc3
bane01 <balazs.nemeth@betssongroup.com>
2020-05-06 10:09:00 +0200

    Chapter 10: Completing the chart

--- SportsStore/SportsStore.Tests/ProductControllerTests.cs
+++ SportsStore/SportsStore.Tests/ProductControllerTests.cs
@@ -11,31 +11,31 @@ namespace SportsStore.Tests
 {
     public class ProductControllerTests
     {
-        [Fact]
-        public void Can_Paginate()
-        {
-            // Arrange
-            Mock<IProductRepository> mock = new Mock<IProductRepository>();
-            mock.Setup(m => m.Products).Returns((new Product[]
-            {
-                new Product { ProductID = 1, Name = "P1" },
-                new Product { ProductID = 2, Name = "P2" },
-                new Product { ProductID = 3, Name = "P3" },
-                new Product { ProductID = 4, Name = "P4" },
-                new Product { ProductID = 5, Name = "P5" }
-            }).AsQueryable<Product>());
+        //[Fact]
+        //public void Can_Paginate()
+        //{
+        //    // Arrange
+        //    Mock<IProductRepository> mock = new Mock<IProductRepository>();
+        //    mock.Setup(m => m.Products).Returns((new Product[]
+        //    {
+        //        new Product { ProductID = 1, Name = "P1" },
+        //        new Product { ProductID = 2, Name = "P2" },
+        //        new Product { ProductID = 3, Name = "P3" },
+        //        new Product { ProductID = 4, Name = "P4" },
+        //        new Product { ProductID = 5, Name = "P5" }
+        //    }).AsQueryable<Product>());
 
-            ProductController controller = new ProductController(mock.Object);
-            controller.PageSize = 3;
+        //    ProductController controller = new ProductController(mock.Object);
+        //    controller.PageSize = 3;
 
-            // Act
-            IEnumerable<Product> result = controller.List(2).ViewData.Model as IEnumerable<Product>;
+        //    // Act
+        //    IEnumerable<Product> result = controller.List(2).ViewData.Model as IEnumerable<Product>;
 
-            // Assert
-            Product[] prodArray = result.ToArray();
-            Assert.True(prodArray.Length == 2);
-            Assert.Equal("P4", prodArray[0].Name);
-            Assert.Equal("P5", prodArray[1].Name);
-        }
+        //    // Assert
+        //    Product[] prodArray = result.ToArray();
+        //    Assert.True(prodArray.Length == 2);
+        //    Assert.Equal("P4", prodArray[0].Name);
+        //    Assert.Equal("P5", prodArray[1].Name);
+        //}
     }
 }
--- SportsStore/SportsStore/Startup.cs
+++ SportsStore/SportsStore/Startup.cs
@@ -27,6 +27,9 @@ namespace SportsStore
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(Configuration["Data:SportsStoreProducts:ConnectionString"])); // Microsoft.EntityFrameworkCore.SqlServer NuGet
             services.AddTransient<IProductRepository, EFProductRepository>();
+            services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddTransient<IOrderRepository, EFOrderRepository>();
             services.AddMvc();
             services.AddMemoryCache();
             services.AddSession();
