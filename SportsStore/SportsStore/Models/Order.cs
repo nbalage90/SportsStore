8ddd238f9eb4894db77480eb47726811f4dc8769
bane01 <balazs.nemeth@betssongroup.com>
2020-05-13 08:27:02 +0200

    Chapter 11 - Administration

--- SportsStore/SportsStore/Models/Order.cs
+++ SportsStore/SportsStore/Models/Order.cs
@@ -13,7 +13,10 @@ namespace SportsStore.Models
         public int OrderID { get; set; }        
         [BindNever]
         public ICollection<CartLine> Lines { get; set; }
-        
+
+        [BindNever]
+        public bool Shipped { get; set; }
+
         [Required(ErrorMessage = "Please enter a name")]
         public string Name { get; set; }
 
